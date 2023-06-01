9aee2c12791be20fa12066ed407dd1cf29bd3ee6
Rahul Umap <Rahul.Umap@Amway.com>
2023-06-01 09:58:14 +0530

    Project setup added

---
+++ NewWorldOrder/NewWorldOrder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewWorldOrder
+//
+//  Created by Rahul Umap on 01/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
