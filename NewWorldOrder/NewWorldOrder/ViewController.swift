c106ac84e3cd46cf470e4893f75e49ea056de554
Rahul Umap <Rahul.Umap@Amway.com>
2023-06-01 12:43:32 +0530

    Speech to text, text to speech added

--- NewWorldOrder/NewWorldOrder/ViewController.swift
+++ NewWorldOrder/NewWorldOrder/ViewController.swift
@@ -6,17 +6,26 @@
 //
 
 import UIKit
+import AVFoundation
+import Speech
 
 class ViewController: UIViewController {
+    let synthesizer = AVSpeechSynthesizer()
+    let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        createGradient()
-        
+       // createGradient()
+        synthesizer.delegate = self
+
+        promptGPT()
+    }
+    
+    func promptGPT() {
         OpenAPIManager.shared.getResponse(input: "help me book an appointment for the chest pain") { result in
             switch result {
-            case .success(let success):
-                print(success)
+            case .success(let text):
+                self.convertToAudio(text)
             case .failure(let failure):
                 print(failure)
             }
@@ -40,6 +49,61 @@ class ViewController: UIViewController {
         // Add gradient layer to the view's layer
         view.layer.insertSublayer(gradientLayer, at: 0)
     }
-
+    
+    
+    @IBAction func audioButtonAction(_ sender: Any) {
+        convertToAudio("We call ourselves 'dreamers and doers' for a reason: we can make happen not just what is possible, but what is impossible. NEOM is a unique investment opportunity, unrivalled anywhere else. This is not business as usual. Be a part of it. Invest in the new future now, invest in NEOM.")
+       // convertToText()
+    }
+    
 }
 
+// MARK: -  Text to Audio & Audio to Text Conversion
+extension ViewController {
+    func convertToAudio(_ text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US") // Set the preferred voice
+        synthesizer.speak(utterance)
+    }
+    
+    func convertToText() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            if authStatus == .authorized {
+                let audioEngine = AVAudioEngine()
+                let request = SFSpeechAudioBufferRecognitionRequest()
+                let node = audioEngine.inputNode
+
+                let recordingFormat = node.outputFormat(forBus: 0)
+                node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+                    request.append(buffer)
+                }
+
+                audioEngine.prepare()
+                do {
+                    try audioEngine.start()
+
+                    self.speechRecognizer?.recognitionTask(with: request) { result, error in
+                        if let transcription = result?.bestTranscription {
+                            let text = transcription.formattedString
+                            print(text)
+                        }
+                    }
+                } catch {
+                    print("Error starting audio engine: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    
+}
+
+extension ViewController: AVSpeechSynthesizerDelegate {
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didCancel utterance: AVSpeechUtterance) {
+        print("Speech synthesis canceled.")
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        print("Speech synthesis finished.")
+    }
+}
