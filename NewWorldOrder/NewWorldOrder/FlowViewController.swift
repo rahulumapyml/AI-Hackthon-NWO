8e22df378bb7477bc3ad435f2898597b3a3d269b
Rahul Umap <rahul.umap@ymedialabs.com>
2023-06-03 17:02:57 +0530

    UI changes

--- NewWorldOrder/NewWorldOrder/FlowViewController.swift
+++ NewWorldOrder/NewWorldOrder/FlowViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 class FlowViewController: UIViewController {
     
     @IBOutlet weak var assistentButton: UIButton!
+    
     override func viewDidLoad() {
-        assistentButton.layer.cornerRadius = assistentButton.frame.height / 2
+        super.viewDidLoad()
     }
     
     @IBAction func assistentButtonAction(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "ViewController")
+        viewController.modalPresentationStyle = .fullScreen
         self.present(viewController, animated: true)
     }
     
--- NewWorldOrder/NewWorldOrder/ViewController.swift
+++ NewWorldOrder/NewWorldOrder/ViewController.swift
@@ -16,15 +16,15 @@ class ViewController: UIViewController {
     @IBOutlet private weak var userInputLabel: UILabel!
     @IBOutlet weak var lottieView: LottieAnimationView!
     @IBOutlet weak var gifBackgroundView: UIImageView!
-
+    @IBOutlet weak var speakButton: UIButton!
+    
     private let userName = "Rahul"
     private let speechRecognitionManager = SpeechRecognitionManager.shared
     private let synthesizer = AVSpeechSynthesizer()
     private var speechTask: DispatchWorkItem?
     
-    var flowType: Flow = .normal
-    
-    var globalText = ""
+    private var flowType: Flow = .normal
+    private var globalText = ""
     
     var isUserTalking = false {
         didSet {
@@ -39,10 +39,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setup()
+
         generativeAIResultLabel.text = flowType.getInitialDialog(userName)
         convertToAudio(flowType.getInitialDialog(userName))
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         
-        setup()
+        speechRecognitionManager.stop()
+        synthesizer.stopSpeaking(at: .immediate)
     }
     
     @IBAction
@@ -52,6 +59,7 @@ class ViewController: UIViewController {
             speechRecognitionManager.stop()
             promptGPT(input: globalText)
         } else {
+            speakButton.setTitle("SPEAK", for: .normal)
             isUserTalking = true
             speechRecognitionManager.start()
         }
@@ -68,6 +76,10 @@ class ViewController: UIViewController {
 private extension ViewController {
     func setup() {
         setUpLottie()
+        isUserTalking = false
+        waveformView.backgroundColor = .clear
+        
+        speakButton.layer.cornerRadius = speakButton.frame.height / 2
         speechRecognitionManager.delegate = self
         generativeAIResultLabel.text = flowType.getInitialDialog(userName)
     }
@@ -84,23 +96,21 @@ private extension ViewController {
     }
 
     func promptGPT(input: String) {
-        print("!@", input)
-
         OpenAPIManager.shared.getResponse(input: input) { result in
             switch result {
             case .success(let text):
+                 let updatedText = text.trimmingCharacters(in: .whitespacesAndNewlines)
                 DispatchQueue.main.async {
-                    self.generativeAIResultLabel.text = text
-                    self.convertToAudio(text)
+                    self.convertToAudio(updatedText)
                 }
             case .failure(let failure):
                 print(failure)
             }
-            
         }
     }
     
     func convertToAudio(_ text: String) {
+
         generativeAIResultLabel.text = text
         let utterance = AVSpeechUtterance(string: text)
         utterance.voice = AVSpeechSynthesisVoice(language: "en-us")
@@ -110,13 +120,15 @@ private extension ViewController {
     
     func updateUI() {
         if isUserTalking {
-            lottieView.isHidden = false
+            speakButton.setTitle("STOP", for: .normal)
+            lottieView.isHidden = true
             generativeAIResultLabel.isHidden = true
             generativeAIResultLabel.text = ""
             userInputLabel.isHidden = false
             waveformView.isHidden = false
         } else {
-            lottieView.isHidden = true
+            speakButton.setTitle("SPEAK", for: .normal)
+            lottieView.isHidden = false
             generativeAIResultLabel.isHidden = false
             userInputLabel.isHidden = true
             userInputLabel.text = ""
